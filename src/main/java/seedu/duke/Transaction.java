3410da10302b6d20a3ff2cf867e7f3c2ec2bfd80
Nishuy52 <jyuc1109sch@gmail.com>
2026-03-09 16:46:19 +0800

    Transaction: Add date field with default today

--- src/main/java/seedu/duke/Transaction.java
+++ src/main/java/seedu/duke/Transaction.java
@@ -1,5 +1,7 @@
 package seedu.duke;
 
+import java.time.LocalDate;
+
 /**
  * Class to be extended for use for income and expense
  */
@@ -8,15 +10,21 @@ public abstract class Transaction {
     protected final String category;
     protected final double amount;
     protected final String description;
+    protected final LocalDate date;
 
-    public Transaction(String category, double amount, String description) {
+    public Transaction(String category, double amount, String description, LocalDate date) {
         this.category = category;
         this.amount = amount;
         this.description = description;
+        this.date = date;
+    }
+
+    public Transaction(String category, double amount, String description) {
+        this(category, amount, description, LocalDate.now());
     }
 
     public Transaction(String category, double amount) {
-        this(category, amount, "");
+        this(category, amount, "", LocalDate.now());
     }
 
     public double getAmount() {
@@ -29,6 +37,10 @@ public abstract class Transaction {
 
     public String getDescription() {
         return description;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 
     public abstract String getType();
