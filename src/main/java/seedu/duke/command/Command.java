451a9f4e0498c820f970a9bfd43ff92f4ce27659
ben <jwben03@gmail.com>
2026-03-16 17:00:42 +0800

    Create an abstract command class

---
+++ src/main/java/seedu/duke/command/Command.java
@@ -0,0 +1,23 @@
+package seedu.duke.command;
+
+import seedu.duke.MoneyBagProMaxException;
+import seedu.duke.transactionlist.TransactionList;
+import seedu.duke.ui.Ui;
+
+public abstract class Command {
+    /**
+     * Executes the command.
+     *
+     * @param list The current list of transactions.
+     * @param ui   The ui instance.
+     */
+    public abstract void execute(TransactionList list, Ui ui) throws MoneyBagProMaxException;
+
+    /**
+     * Indicates whether this command should exit the application.
+     * Defaults to false, but the ExitCommand will override this to return true to exit.
+     */
+    public boolean isExit() {
+        return false;
+    }
+}
